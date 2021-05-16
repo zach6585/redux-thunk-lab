4f76c2f46c5ba5255da95e19dd6be6801fe8225a
zach6585 <zmines@wesleyan.edu>
2021-05-16 19:50:24 -0400

    commit tree 8ba2d2d555e351af11e8311618507657dfd6ef4b

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux';
 import App from './App'
+import { thunk } from 'redux-thunk';
+import { Provider } from 'react-redux';
+import catsReducer from './reducers/catsReducer';
 
-
+const store = createStore(catsReducer, applyMiddleware(thunk));
 ReactDOM.render(
-    <App />,
+    < Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
